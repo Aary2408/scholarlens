0e5dfb0a09fd0539bc9a01f74687889780f8ed29
Aary Gaikwad <aarygaikwad86@gmail.com>
2026-09-06 09:09:06 +0000

    Fix duplicate AdSense script

--- app/layout.js
+++ app/layout.js
@@ -106,14 +106,7 @@ gtag('config', 'G-L7M0XLQZS9');`}
         <Script id="ld-organization" type="application/ld+json" strategy="beforeInteractive" dangerouslySetInnerHTML={{ __html: JSON.stringify(orgJsonLd) }} />
         <Script id="ld-website" type="application/ld+json" strategy="beforeInteractive" dangerouslySetInnerHTML={{ __html: JSON.stringify(siteJsonLd) }} />
       </head>
-      <body>{children}<CookieNotice />
-      <Script
-  async
-  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9144354248628915"
-  crossOrigin="anonymous"
-  strategy="afterInteractive"
-/> 
-</body>
+      <body>{children}<CookieNotice /></body>
     </html>
   );
 }
